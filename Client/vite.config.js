fbc28e124c856b92e1edcec6ddae65ccebd0fafd
Nechama05 <Hand4d21@mbjcomp.org.il>
2025-06-24 01:35:12 +0300

    ע

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -10,16 +10,27 @@
 //   },
 // })
 
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3001,  // frontend על פורט 3001
+//     proxy: {
+//       '/api': 'http://localhost:3000',  // כל קריאה ל/api תועבר לשרת backend בפורט 3000
+//     },
+//   },
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3001,  // frontend על פורט 3001
     proxy: {
-      '/api': 'http://localhost:3000',  // כל קריאה ל/api תועבר לשרת backend בפורט 3000
-    },
-  },
+      '/api': 'http://localhost:3000'
+    }
+  }
 })
